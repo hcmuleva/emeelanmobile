dc3789c293bef10b7a923d314d9344b8e8614a69
Harish Muleva <harishmuleva@apples-MacBook-Pro.local>
2024-12-05 19:54:31 +0530

    Profile addtional details for display created and need to fix for edit profile option for the same

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,12 @@ import { axiosInstance } from "./config/axiosInstance";
 import { resourcesConfig } from "./config/resources";
 import { PageViewProvider } from "./contextprovider/PageProvider";
 import Controller from "./pages/Controller";
-import LoginPage from "./pages/login/LoginPage";
+//import LoginPage from "./pages/login/LoginPage";
 import MatchesPage from "./pages/matches";
 import { RegisterPage } from "./pages/register/register";
 import MyProfile from "./pages/myProfile";
 import HelpPage from "./help/HelpPage";
+import LoginPage from "./pages/login/Loginpage";
 
 export const TOKEN_KEY = import.meta.env.VITE_TOKEN_KEY;
 const API_URL = import.meta.env.VITE_SERVER_URL;
