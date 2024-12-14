a3029cb338cf4495e574ebeaf24205b1bc38aea1
ybarfa <barfayash2@gmail.com>
2024-12-14 13:28:14 +0530

    Mellan users will now get list of approved users only

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,39 +1,37 @@
-import { useCustom, useList } from '@refinedev/core';
-import React, { useState } from 'react'
+import { useCustom } from '@refinedev/core';
+import React, { useState } from 'react';
 import UserTableView from './UserDashboard/UserTableView';
-import Header from './Header';
+
 const API_URL = import.meta.env.VITE_SERVER_URL;
 
 export default function UserDashboard() {
-  console.log("Default in userDashboard")
     const [current, setCurrent] = useState(1);
     const [pageSize] = useState(20);
+
     const { data, isLoading, isFetching, refetch } = useCustom({
         url: `${API_URL}/api/custom-user`,
         method: "get",
         config: {
-          headers: {
-            "x-custom-header": "foo-bar",
-            Authorization: `Bearer ${localStorage.getItem("jwt-token")}`,
-          },
+            headers: {
+                "x-custom-header": "foo-bar",
+                Authorization: `Bearer ${localStorage.getItem("jwt-token")}`,
+            },
         },
         query: {
             pagination: { page: current, pageSize },
         }
-      });
+    });
 
-      if(isLoading){
-        return <h1>Page Loading</h1>
-      }
-      console.log("data",data)
-      return (<>
-      
-      <UserTableView rowData={data?.data?.data}/>
-      </>)
-  return (
-    <div>
-      <h1>UserDashboard</h1>
-      
-    </div>
-  )
+    if (isLoading) {
+        return <h1>Page Loading...</h1>;
+    }
+
+    // Filter for users with 'APPROVED' status
+    const approvedUsers = data?.data?.data?.filter(user => user.userstatus === "APPROVED");
+
+    return (
+        <>
+            <UserTableView rowData={approvedUsers} refetch={refetch} />
+        </>
+    );
 }
