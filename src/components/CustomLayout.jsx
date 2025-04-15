eed744d4926c3f1f48a081cbe2a88bec13e4fe8d
hcmuleva <hcmuleva@gmail.com>
2025-04-15 08:59:32 +0530

    Removed old version of emeelan

--- src/components/CustomLayout.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CustomLayout = ({ children }) => {
-    return <div style={{ padding: "16px" }}>{children}</div>;
-};
-
-export default CustomLayout;
--- src/pages/UserDashboard/userview/profilestatus/PendingProfile.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function PendingProfile() {
-  return (
-    <div>
-      <h1>Pending Request</h1>
-    </div>
-  )
-}
