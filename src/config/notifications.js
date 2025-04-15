a80e1369e089d785f2506baef1a5fa007cf96371
hcmuleva <hcmuleva@gmail.com>
2025-04-15 08:59:32 +0530

    Removed old version of emeelan

--- src/config/notifications.js
+++
@@ -1,8 +0,0 @@
-export const useNotificationProvider = {
-    open: ({ type, message, description }) => {
-        console.log(`[${type}] ${message}: ${description}`);
-    },
-    close: (key) => {
-        console.log(`Closing notification ${key}`);
-    },
-};
