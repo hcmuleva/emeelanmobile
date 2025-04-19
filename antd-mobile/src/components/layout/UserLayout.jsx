763491399b91a2c83703e88e286003ccc05a7545
Dhruv Rathore <dhruvrathore3002@gmail.com>
2025-04-19 20:29:09 +0530

    ui fixes aaded viewport-fit cover in public index.html

--- antd-mobile/src/components/layout/UserLayout.jsx
+++ antd-mobile/src/components/layout/UserLayout.jsx
@@ -35,54 +35,60 @@ export default function UserLayout({ children }) {
   ];
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+    <div style={{
+      display: "flex", flexDirection: "column", minHeight: "100vh"
+    }}>
       <TopBar userRole={"User"} />
       <div style={{ flex: 1, overflowY: "auto" }}>{children}</div>
 
       {/* StatusNotification hooks into Ably */}
+
       <StatusNotification userId={userId} setNotificationStats={setNotificationStats} />
 
-      <div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-around",
-            padding: "12px 0",
-            backgroundColor: "white",
-            borderTopLeftRadius: "20px",
-            borderTopRightRadius: "20px",
-            boxShadow: "0 -2px 10px rgba(0, 0, 0, 0.05)",
-            position: "fixed",
-            bottom: 0,
-            width: "100%",
-            zIndex: 100,
-            paddingBottom: 'env(safe-area-inset-bottom)'
-          }}
-        >
-          {footerItems.map((item) => {
-            const isActive = location.pathname === `/${item.key}`;
-            return (
-              <div
-                key={item.key}
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  color: isActive ? "#FF1E56" : "#999",
-                  paddingBottom: 'env(safe-area-inset-bottom)'
-                }}
-                onClick={() => navigate(`/${item.key}`)}
-              >
-                {item.icon}
-                <div style={{ fontSize: 12, marginTop: 4 }}>
-                  {item.key.charAt(0).toUpperCase() + item.key.slice(1)}
-                </div>
-              </div>
-            );
-          })}
-        </div>
+      {/* <div>
         <div style={{ height: "70px" }} />
+      </div> */}
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "start",
+          backgroundColor: "white",
+          boxShadow: "0 -2px 10px rgba(0, 0, 0, 0.05)",
+          position: "sticky",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          width: "100%",
+          zIndex: 100,
+          padding: "16px 0",
+          height: "80px",
+          paddingBottom: 'env(safe-area-inset-bottom)'
+        }}
+      >
+        {footerItems.map((item) => {
+          const isActive = location.pathname === `/${item.key}`;
+          return (
+            <div
+              key={item.key}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                color: isActive ? "#FF1E56" : "#999",
+                padding: "10px",
+              }}
+              onClick={() => navigate(`/${item.key}`)}
+            >
+              {item.icon}
+              <div style={{ fontSize: 12, marginTop: 4 }}>
+                {item.key.charAt(0).toUpperCase() + item.key.slice(1)}
+              </div>
+            </div>
+          );
+        })}
       </div>
-    </div>
+    </div >
   );
 }
